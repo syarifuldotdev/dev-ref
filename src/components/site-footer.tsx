822997f26e799c93397e1945268579d8b157ac90
Syariful Kamaluddin <syariful.dev@gmail.com>
2023-08-19 19:14:36 +0800

    V0.1.18

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -7,9 +7,10 @@ export function SiteFooter() {
         <footer className="py-6 md:px-8 md:py-0">
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                 <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+                    <div>Lot More To Be Added</div>
                     <div>Missing Useful Website? <Link className="font-medium underline underline-offset-4" href={siteConfig.links.github}>Contribute Here</Link>.</div>
                     <div>
-                        Built by{" "}
+                        Built By{" "}
                         <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">syarifuldotdev</Link>.
                     
                     </div>
